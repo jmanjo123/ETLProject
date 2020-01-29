cc4b187efe41f3a7735c61f196f26b027b56a1e4
maria patino <mariavpatino@hotmail.com>
2020-01-28 23:57:28 -0500

    Schema New Version

--- Resources/schema.sql
+++ Resources/schema.sql
@@ -1,11 +1,18 @@
 -- Create tables for raw data to be loaded into
+
+DROP TABLE if exists listings_locations
+DROP TABLE  if exists listings_details;
+DROP  TABLE  if exists url;
+DROP  TABLE  if exists hosts;
+
+
 CREATE TABLE listings_locations (
 id INT PRIMARY KEY,
 city TEXT,
 state TEXT,
-zipcode INT,
-latitude INT,
-longitude INT);
+zipcode TEXT,
+latitude FLOAT,
+longitude FLOAT);
 
 
 CREATE TABLE listings_details (
@@ -20,7 +27,7 @@ PRIMARY KEY (id),
 FOREIGN KEY (id) REFERENCES listings_locations(id));
 
 
-CREATE TABLE Url (
+CREATE TABLE url (
 id INT,
 listing_url TEXT,
 picture_url TEXT,
@@ -28,7 +35,7 @@ PRIMARY KEY (id),
 FOREIGN KEY (id) REFERENCES  listings_locations(id));
 
 
-CREATE TABLE Hosts (
+CREATE TABLE hosts (
 id INT,
 host_id INT,
 host_url TEXT,
@@ -40,4 +47,7 @@ PRIMARY KEY (id),
 FOREIGN KEY (id) REFERENCES listings_locations(id));
 
 
-SELECT * FROM Hosts
+SELECT * FROM url
+SELECT * FROM hosts
+SELECT * FROM listings_locations
+SELECT * FROM listings_details
